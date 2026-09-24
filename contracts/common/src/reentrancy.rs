525f3c3a9c1bdf55a0c1de30c98960298ba88d77
gabbydunkk <grizygjunor@gmail.com>
2026-09-24 10:58:36 +0100

    Fix assigned issues: 375, 385, 386, 388

---
+++ contracts/common/src/reentrancy.rs
@@ -0,0 +1,24 @@
+use soroban_sdk::{contracttype, Env};
+use crate::Error;
+
+#[contracttype]
+pub enum ReentrancyDataKey {
+    Lock,
+}
+
+pub struct ReentrancyGuard;
+
+impl ReentrancyGuard {
+    pub fn acquire(env: &Env) -> Result<(), Error> {
+        let is_locked: bool = env.storage().instance().get(&ReentrancyDataKey::Lock).unwrap_or(false);
+        if is_locked {
+            return Err(Error::ReentrancyBlocked);
+        }
+        env.storage().instance().set(&ReentrancyDataKey::Lock, &true);
+        Ok(())
+    }
+
+    pub fn release(env: &Env) {
+        env.storage().instance().set(&ReentrancyDataKey::Lock, &false);
+    }
+}
